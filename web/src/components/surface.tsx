a19fd1313016e8c15661b2e091ff6e7ea84ab8d9
Munawar Shah <93397215+mwrshah@users.noreply.github.com>
2026-04-03 17:25:08 +0500

    input panel: reduce minSize to 7%

--- web/src/components/surface.tsx
+++ web/src/components/surface.tsx
@@ -837,7 +837,7 @@ export function Surface() {
 
         <HorizontalResizeHandle />
 
-        <Panel defaultSize="15%" minSize="12%">
+        <Panel defaultSize="15%" minSize="7%">
           <MessageInput
             isSending={isSending}
             onSubmit={handleSubmit}
